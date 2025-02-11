67579a30fff3f1fed9efcde75de5756bcacf1278
Joyuv <givanisonmariz7@outlook.com>
2025-02-11 14:10:03 -0300

    problemas concertados, overflow menu e css da criação de personagem inciada

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,6 +1,6 @@
 function trocarCss(id){
     const style = document.getElementById("style")
     if (id === "iniciar"){
-        style.href = "scr/styles/game.css"
+        style.href = "src/styles/criaperso.css"
     }
 }
