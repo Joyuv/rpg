ff59d169c5b829bbac2f512c786368df5b098b3e
Joyuv <givanilsonmariz7@outlook.com>
2025-02-20 18:06:10 -0300

    lobo ataqueUm e adicionado múltipla rolagem de dados

---
+++ src/scripts/modules/Lobo.js
@@ -0,0 +1,21 @@
+import Entidade from "./Entidade"
+import { d20, d6 } from "./dados"
+
+module.exports = class Lobo extends Entidade {
+
+  constructor(nome, nivel, forca, destreza, resistencia) {
+    super(nome, nivel, forca, destreza, resistencia)
+  }
+
+  ataqueUm(resis) {
+    const tentativa = d20(1) + this.destreza
+    if (tentativa > resis) {
+      const dano = d6(math.floor(this.nivel / 2) + this.forca)
+      return [dano, "O lobo morde seu braço e arranca um pouco de pele"]
+    }
+    else {
+      return "O lobo pula em sua direção mas você consegue desviar de seu ataque"
+    }
+  }
+
+}
--- src/scripts/modules/dados.js
+++ src/scripts/modules/dados.js
@@ -1,9 +1,17 @@
-function d20(){
-    return Math.floor(Math.random() * 20)
+function d20(quant) {
+    const resultadoFinal = 0
+    for (i = 0; i < quant; i++) {
+        resultadoFinal += Math.floor(Math.random() * 20)
+    }
+    return resultadoFinal
 }
 
-function d6(){
-    return Math.floor(Math.random() * 6)
+function d6(quant) {
+    const resultadoFinal = 0
+    for (i = 0; i < quant; i++) {
+        resultadoFinal += Math.floor(Math.random() * 6)
+    }
+    return resultadoFinal
 }
 
 module.exports = {
